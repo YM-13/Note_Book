7d968658063cf9811f70638e01c9bad1b6dc50ee
YM <exitospec1@gmail.com>
2022-06-14 19:24:14 +0200

    model prepared

--- note_app/models.py
+++ note_app/models.py
@@ -1,13 +1,14 @@
+from datetime import datetime
 from django.db import models
+
 
 # Create your models here.
 class Note(models.Model):
-    pass
-    id = models.BigAutoField(primary_key=True) # (integer значение)
-    description = models. # (text field) 
-    title = models.CharField(_("ОГЛАВЛЕНИЕ"), max_length=256)
-    created_at (datetime, значение должно запис. автома. при создании объекта) = models.
-    updated_at (datetime, также записывается автоматически, но в при каждом обновлении данных) = models.
-    updated_at (datetime, также записывается автоматически, но в при каждом обновлении данных) = models.
-    # добавить сортировку по-умолчанию по полю title
+    id = models.IntegerField()  # (integer значение)
+    # id = models.BigAutoField(primary_key=True)
+    description = models.TextField() # (text field) 
+    title = models.CharField("ОГЛАВЛЕНИЕ", max_length=256)
+    created_at = models.DateTimeField(auto_now=True) #(datetime, значение должно запис. автома. при создании объекта)
+    updated_at = models.DateTimeField(auto_now_add=True) #(datetime, также записывается автоматически, но при каждом обновлении данных)
+Note.objects.all().order_by('title') # добавить сортировку по-умолчанию по полю title
     # добавить кастомное отображение в строке: "<Note(id={id объекта}, title={title модели}>"
