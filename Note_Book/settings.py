56abb0f14fc813c859b2320a4d214263e584fe27
YM <exitospec1@gmail.com>
2022-06-14 17:45:01 +0200

    registration of application 'note_app'

--- Note_Book/settings.py
+++ Note_Book/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'note_app.apps.NoteAppConfig', # registration of application "note_app"
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- note_app/models.py
+++ note_app/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Note(models.Model):
     pass
-    # id (integer значение) = models.BigAutoField(primary_key=True)
-    # description (text field) = models.
-    # title = models.CharField(_("ОГЛАВЛЕНИЕ"), max_length=256)
-    # created_at (datetime, значение должно запис. автома. при создании объекта) = models.
-    # updated_at (datetime, также записывается автоматически, но в при каждом обновлении данных) = models.
-    # updated_at (datetime, также записывается автоматически, но в при каждом обновлении данных) = models.
+    id = models.BigAutoField(primary_key=True) # (integer значение)
+    description = models. # (text field) 
+    title = models.CharField(_("ОГЛАВЛЕНИЕ"), max_length=256)
+    created_at (datetime, значение должно запис. автома. при создании объекта) = models.
+    updated_at (datetime, также записывается автоматически, но в при каждом обновлении данных) = models.
+    updated_at (datetime, также записывается автоматически, но в при каждом обновлении данных) = models.
     # добавить сортировку по-умолчанию по полю title
     # добавить кастомное отображение в строке: "<Note(id={id объекта}, title={title модели}>"
