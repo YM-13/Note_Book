416aa3054cb368ca4671ea8aa6d8e72878103a71
YM <exitospec1@gmail.com>
2022-06-17 09:37:36 +0200

    delete comments

--- note_app/models.py
+++ note_app/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 
 # Create your models here.
-class Note(models.Model):
-    id = models.IntegerField()  # (integer значение)
-    # id = models.BigAutoField(primary_key=True)
+class Note(models.Model): 
+    id = models.BigAutoField(primary_key=True)
     description = models.TextField() # (text field) 
-    title = models.CharField("ОГЛАВЛЕНИЕ", max_length=256)
+    title = models.CharField("ОГЛАВЛЕНИЕ", max_length=256) # Engl translate
     created_at = models.DateTimeField(auto_now=True) #(datetime, значение должно запис. автома. при создании объекта)
     updated_at = models.DateTimeField(auto_now_add=True) #(datetime, также записывается автоматически, но при каждом обновлении данных)
+
 Note.objects.all().order_by('title') # добавить сортировку по-умолчанию по полю title
     # добавить кастомное отображение в строке: "<Note(id={id объекта}, title={title модели}>"
