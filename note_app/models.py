fea1cbca2e266d086f4131dda66f7de846ebe26b
YM <exitospec1@gmail.com>
2022-06-18 17:29:37 +0200

    in models.py created 'default ordering by TITLE' and method __str__

--- note_app/models.py
+++ note_app/models.py
@@ -5,10 +5,15 @@ from django.db import models
 # Create your models here.
 class Note(models.Model): 
     id = models.BigAutoField(primary_key=True)
-    description = models.TextField() # (text field) 
-    title = models.CharField("ОГЛАВЛЕНИЕ", max_length=256) # Engl translate
-    created_at = models.DateTimeField(auto_now=True) #(datetime, значение должно запис. автома. при создании объекта)
-    updated_at = models.DateTimeField(auto_now_add=True) #(datetime, также записывается автоматически, но при каждом обновлении данных)
+    description = models.TextField()
+    title = models.CharField("TITLE", max_length=256)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
-Note.objects.all().order_by('title') # добавить сортировку по-умолчанию по полю title
-    # добавить кастомное отображение в строке: "<Note(id={id объекта}, title={title модели}>"
+
+    class Meta:
+        ordering = ['title']
+
+
+    def __str__(self):
+        return self.id, self.title
